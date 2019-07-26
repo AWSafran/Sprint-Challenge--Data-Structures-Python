d2762c036ee4640d8e3ad206b14babae4135803e
AWSafran <andrewsafran@gmail.com>
2019-07-26 12:14:02 -0400

    implemented O(n) method for names

--- names/names.py
+++ names/names.py
@@ -22,3 +22,27 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+print("Second pass solution below")
+
+new_time = time.time()
+f = open('names_1.txt', 'r')
+first_names = f.read().split("\n")
+f.close
+
+f = open('names_2.txt', 'r')
+second_names = f.read().split("\n")
+f.close
+
+name_dict = {}
+duplicate_list = []
+
+for first_name in first_names:
+    name_dict[first_name] = 1
+for second_name in second_names:
+    if name_dict.get(second_name) is not None:
+        duplicate_list.append(second_name)
+
+new_end = time.time()
+print (f"{len(duplicate_list)} duplicates:\n\n{', '.join(duplicate_list)}\n\n")
+print (f"runtime: {new_end - new_time} seconds")
+
