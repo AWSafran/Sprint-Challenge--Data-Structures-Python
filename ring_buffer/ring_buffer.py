fd689746504b09f6faf89ab002dd02ce55b467aa
AWSafran <andrewsafran@gmail.com>
2019-07-26 11:24:14 -0400

    implemented get method, all tests passed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -14,19 +14,8 @@ class RingBuffer:
 
 
   def get(self):
-    print(f"Printing the list now, starting with current at {self.current}")
-    print(self.storage)
-    buffer_list = []
-    append_index = self.current
+    rtn_list = []
     for i in range(0, len(self.storage)):
-      print(f"current is {self.current}")
-      if append_index >= len(self.storage) -1:
-        append_index = 0
-      else:
-        append_index += 1
-      if self.storage[append_index]:
-        print(f"Current is {append_index}, appending {self.storage[append_index]}")
-        buffer_list.append(self.storage[append_index])
-    print(f"finished printing with current at {append_index}")
-
-    return buffer_list
+      if self.storage[i]:
+        rtn_list.append(self.storage[i])
+    return rtn_list
